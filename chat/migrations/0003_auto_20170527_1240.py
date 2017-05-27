8590f7e051001ec659d9024c7c8f5b94c740e8b1
unknown <panna1997@yandex.ru>
2017-05-27 20:50:01 +0300

    Migration fixed

--- chat/migrations/0003_auto_20170527_1240.py
+++ chat/migrations/0003_auto_20170527_1240.py
@@ -33,7 +33,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='message',
             name='user',
-            field=models.ForeignKey(default='exit', on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL),
+            field=models.ForeignKey(default=1, on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL),
             preserve_default=False,
         ),
         migrations.DeleteModel(
