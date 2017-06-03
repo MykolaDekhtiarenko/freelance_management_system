13b0621c759ee35b0362a2597f63faaa4dbd9b42
g_f0x <alex.frankiv13@gmail.com>
2017-06-03 15:15:28 +0300

    my_applications & profile web pages (generic, no styles yet)

--- web/urls.py
+++ web/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import url
 from web.views import home
 from django.contrib.auth import views
-from web.views import AllProjectsListView
+from web.views import *
 # We are adding a URL called /home
 urlpatterns = [
+    url(r'applications', MyApplicationsListView.as_view()),
+    url(r'portfolio', PortfolioDetailView.as_view()),
     url(r'^projects', AllProjectsListView.as_view()),
     url(r'^$', home),
     url(r'^$', home),
--- web/views.py
+++ web/views.py
@@ -1,10 +1,13 @@
+from django.http import Http404
 from django.shortcuts import render
 
 # Create your views here.
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.views.generic import ListView
-from api.models import Project
+from django.views.generic import DetailView
+from api.models import *
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 @login_required(login_url="/login")
@@ -14,3 +17,21 @@ def home(request):
 class AllProjectsListView(ListView):
     model = Project
     template_name = "web/allprojects.html"
+
+class MyApplicationsListView(LoginRequiredMixin, ListView):
+    template_name = "web/myapplications.html"
+    paginate_by = 10
+    def get_queryset(self):
+        return Application.objects.filter(user=self.request.user)
+
+class PortfolioDetailView(LoginRequiredMixin, DetailView):
+    template_name = "web/portfolio.html"
+    def get_queryset(self):
+        if not self.request.user.is_superuser:
+            u_role = self.request.user.profile.role
+            if u_role == Profile.RoleValues.developer:
+                return Portfolio.objects.filter(user=self.request.user)
+            else:
+                raise Http404("No portfolio is available for users who are %s" % u_role)
+        else:
+            raise Http404("No portfolio is available for superusers")
