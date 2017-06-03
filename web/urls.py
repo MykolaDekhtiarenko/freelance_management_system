165d9bcaee7d5168ffb0a78c45ce66c4dd766a11
Mykola Dekhtiarenko <nnick1212@gmail.com>
2017-06-03 15:17:13 +0300

    added my projects list view

--- web/urls.py
+++ web/urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls import url
 from web.views import home
 from django.contrib.auth import views
-from web.views import AllProjectsListView
+from web.views import *
 # We are adding a URL called /home
 urlpatterns = [
     url(r'^projects', AllProjectsListView.as_view()),
-    url(r'^$', home),
+    url(r'^myprojects', MyProjectsListView.as_view()),
     url(r'^$', home),
     url(r'^login/$', views.login, {'template_name': 'registration/login.html'}, name='login'),
     url(r'^logout/$', views.logout, {'next_page': '/login'}),
--- web/views.py
+++ web/views.py
@@ -1,16 +1,35 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import Http404
 from django.shortcuts import render
 
 # Create your views here.
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.views.generic import ListView
-from api.models import Project
+from api.models import *
 
 
 @login_required(login_url="/login")
 def home(request):
     return render(request, "web/home.html")
 
-class AllProjectsListView(ListView):
+class AllProjectsListView(LoginRequiredMixin, ListView):
     model = Project
     template_name = "web/allprojects.html"
+    paginate_by = 10
+
+class MyProjectsListView(LoginRequiredMixin, ListView):
+    template_name = "web/myprojects.html"
+    paginate_by = 10
+
+    def get_queryset(self):
+        if not self.request.user.is_superuser:
+            if self.request.user.profile.role == Profile.RoleValues.creator:
+                return Project.objects.filter(creator = self.request.user)
+            elif self.request.user.profile.role == Profile.RoleValues.developer:
+                # TODO add filter by status of application
+                return Project.objects.filter(applications__user=self.request.user)
+        else:
+            raise Http404("No portfolio is available for superuser")
+
+
