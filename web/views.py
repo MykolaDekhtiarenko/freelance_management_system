0b97487efb609fa8e55a3330cead8c56fca8a600
Mykola Dekhtiarenko <nnick1212@gmail.com>
2017-06-03 14:09:30 +0300

    projects list view added

--- web/views.py
+++ web/views.py
@@ -3,9 +3,14 @@ from django.shortcuts import render
 # Create your views here.
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.views.generic import ListView
+from api.models import Project
 
 
 @login_required(login_url="/login")
 def home(request):
     return render(request, "web/home.html")
 
+class AllProjectsListView(ListView):
+    model = Project
+    template_name = "web/allprojects.html"
