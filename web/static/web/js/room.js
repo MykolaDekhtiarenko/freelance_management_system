ec4deee6bcbdea0a799c40c5966115de3978a3ee
unknown <panna1997@yandex.ru>
2017-06-17 16:54:06 +0300

    Chat fixed, layout prepared

--- web/static/web/js/room.js
+++ web/static/web/js/room.js
@@ -1,20 +1,33 @@
 $(document).ready(function(){ 
     $("#toggle").click(function(event){
-        $(".сhat-room").animate({width:'toggle'},350);
+        $(".сhat-room").animate({width:'toggle'},300);
             var hidden = $('.chat-room');
              if (hidden.hasClass('visible')){
-                 setTimeout(function(){   $(".task-grid").removeClass("col-sm-12").addClass("col-sm-9")},600);
-             
-
-                 hidden.animate({"right":"0px"}, "slow").removeClass('visible');
-                 $("#toggle").animate({"right":"100%"}, "slow").removeClass('visible');
+                 $(".task-grid").removeClass("col-sm-12").addClass("col-sm-9");
+                 hidden.animate({"right":"0px"}, 300).removeClass('visible');
+                 $("#toggle").animate({"right":"90%"}, 300).removeClass('visible');
                  $(this).toggleClass('glyphicon glyphicon-menu-left glyphicon glyphicon-menu-right');
              } else {
-                                  $(".task-grid").removeClass("col-sm-9").addClass("col-sm-12");
-
-                 hidden.animate({"right":"-90%"}, "slow").addClass('visible');
-                 $("#toggle").animate({"right":"10%"}, "slow").addClass('visible');
+                 $(".task-grid").removeClass("col-sm-9").addClass("col-sm-12");
+                 hidden.animate({"right":"-85%"}, 300).addClass('visible');
+                 $("#toggle").animate({"right":"5%"}, 300).addClass('visible');
                  $(this).toggleClass('glyphicon glyphicon-menu-left glyphicon glyphicon-menu-right');
              }
     });
-}); 
+    var modal = $('#myModal');
+    var btn = $(".details");
+    var span=$(".close");
+    btn.click(function(){
+        modal.css("display","block");
+    });
+    span.click(function(){
+        modal.css("display","none");
+    });
+    window.click=function(event){
+        if(event.target==modal){
+            modal.css("display","none");
+        }
+    }
+});
+
+
