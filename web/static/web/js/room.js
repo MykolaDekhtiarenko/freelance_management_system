f0d9f1e0825cb0a340046559e5208a5aed26d27d
unknown <panna1997@yandex.ru>
2017-06-19 20:54:59 +0300

    Chat room improved a lot

--- web/static/web/js/room.js
+++ web/static/web/js/room.js
@@ -1,5 +1,5 @@
-$(document).ready(function(){ 
-    $("#toggle").click(function(event){
+$(document).ready(function(event,event2){
+    $("#toggle").click(function(){
         $(".сhat-room").animate({width:'toggle'},300);
             var hidden = $('.chat-room');
              if (hidden.hasClass('visible')){
@@ -18,16 +18,39 @@ $(document).ready(function(){
     var btn = $(".details");
     var span=$(".close");
     btn.click(function(){
+        modal.fadeIn(300);
         modal.css("display","block");
     });
     span.click(function(){
-        modal.css("display","none");
+        modal.fadeOut(200);
     });
     window.click=function(event){
         if(event.target==modal){
+            modal.fadeOut(200);
             modal.css("display","none");
+
+        }
+        event.stopPropagation();
+    }
+    var modal2 = $('#addTask');
+    var btn2 = $("#add");
+    var span2=$(".close");
+    btn2.click(function(){
+        modal2.fadeIn(300);
+        modal2.css("display","block");
+    });
+    span2.click(function(){
+        modal2.fadeOut(200);
+    });
+    window.click=function(event2){
+        if(event2.target==modal2){
+            modal2.fadeOut(200);
+            modal2.css("display","none");
+            event2.stopPropagation();
         }
     }
+    var myDiv = $('.description');
+    myDiv.text(myDiv.text().substring(0,30));
+    myDiv.append("...");
 });
-
 
