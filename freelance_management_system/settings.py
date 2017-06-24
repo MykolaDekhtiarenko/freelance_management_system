ab3278d62d229d50b43c442321d85ed87a8aaac7
Mykola Dekhtiarenko <nnick1212@gmail.com>
2017-06-24 14:41:21 +0300

    db changed

--- freelance_management_system/settings.py
+++ freelance_management_system/settings.py
@@ -88,7 +88,7 @@ TEMPLATES = [
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(default="postgres:///postgresql-asymmetrical-77136", conn_max_age=500)
+    'default': dj_database_url.config(default="postgres:///postgresql-globular-28914", conn_max_age=500)
     # 'default': {
     #     'ENGINE': 'django.db.backends.mysql',
     #     'NAME': 'freelance_ms',
